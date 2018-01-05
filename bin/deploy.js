167c7ee50fadd48c61cbdd4fee7470ebff2585e4
fangtao04 <fangtao04@meituan.com>
2018-01-05 11:49:51 +0800

    为何md里包含require时会被解析???

--- bin/deploy.js
+++ bin/deploy.js
@@ -5,7 +5,12 @@ import article from '../src/data/article'
 Object.entries(article).forEach(([k, v]) => {
     v.forEach(v2 => {
         const {name} = v2
-        const content = fs.readFileSync(`src/data/article/node/fs.md`).toString()
+        const content = fs.readFileSync(`src/data/article/${k}/${name}.md`).toString().split('\n').filter(v => !v.includes('require')).join('\n')
+
+        if(name === 'Tinify.js') {
+            console.log(content)
+        }
+
         const toFilePath = `src/data/articleJS/${k}/${name}.js`
         fs.ensureFileSync(toFilePath)
         fs.writeFileSync(toFilePath, 'export default ' + JSON.stringify(content))
